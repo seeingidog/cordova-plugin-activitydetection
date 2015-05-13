ce1a6f5754b5c96bcc8bf701b0dd31bdf874d57f
Ian Morgan <ianm280@gmail.com>
2015-05-13 00:55:57 -0700

    Rough first stab at Android native code, clean up JS interface

--- src/android/ActivityDetectionPlugin.java
+++ src/android/ActivityDetectionPlugin.java
@@ -1,13 +1,47 @@
 package org.seeingidog.activitydetection;
 
-package org.apache.cordova.plugin;
-
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
 
 public class ActivityDetectionPlugin extends CordovaPlugin {
+  protected void pluginInitialize() {
+  }
+  
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
+    throws JSONException {
+    if (action.equals("requestActivityUpdate")) {
+      return true;
+    }
+    return false;
+  }
+    
+  public class ActivityRecognitionIntentService extends IntentService { 
+      @Override
+      protected void onHandleIntent(Intent intent) {
+            // If the intent contains an update
+          if (ActivityRecognitionResult.hasResult(intent)) {
+              // Get the update
+              ActivityRecognitionResult result =
+                ActivityRecognitionResult.extractResult(intent);
+ 
+               DetectedActivity mostProbableActivity
+                      = result.getMostProbableActivity();
+ 
+               // Get the confidence % (probability)
+              int confidence = mostProbableActivity.getConfidence();
+  
+              // Get the type
+              int activityType = mostProbableActivity.getType();
+
+              callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, activityType));              
+            }
+        }
+    }
+  }
+  
 }
--- www/cordova-plugin-activitydetection.js
+++ www/cordova-plugin-activitydetection.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.requestActivityUpdates = function(arg0, success, error) {
-    exec(success, error, "cordova-plugin-activitydetection", "requestActivityUpdates");
-};
+function requestActivityUpdates() {
+    exec(null, null, "ActivityDetectionPlugin", "requestActivityUpdate", []);
+}
